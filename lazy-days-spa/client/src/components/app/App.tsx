fb6ecefeaf565d63f74ae4211e7cb48ce1cea78c
dltlaos11 <dltlaos14@naver.com>
2023-12-05 20:45:06 +0900

    adding file

--- lazy-days-spa/client/src/components/app/App.tsx
+++ lazy-days-spa/client/src/components/app/App.tsx
@@ -1,6 +1,11 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import { ReactElement } from 'react';
+import { QueryClientProvider } from 'react-query';
+// provider는 자식들이 react-query 캐시와 queryClient에 접속할 수 있게 함
+// 그리고 queryClient를 프로퍼티로 사용
+import { ReactQueryDevtools } from 'react-query/devtools';
 
+import { queryClient } from '../../react-query/queryClient';
 import { theme } from '../../theme';
 import { Loading } from './Loading';
 import { Navbar } from './Navbar';
@@ -9,9 +14,12 @@ import { Routes } from './Routes';
 export function App(): ReactElement {
   return (
     <ChakraProvider theme={theme}>
-      <Navbar />
-      <Loading />
-      <Routes />
+      <QueryClientProvider client={queryClient}>
+        <Navbar />
+        <Loading />
+        <Routes />
+        <ReactQueryDevtools />
+      </QueryClientProvider>
     </ChakraProvider>
   );
 }
