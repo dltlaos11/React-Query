d05d91186097e919a06b55a3c25176123d43aabd
dltlaos11 <dltlaos14@naver.com>
2023-12-06 22:42:24 +0900

    feat: 커스텀 쿼리 훅(쿼리키의 일관성-다수 키의 혼동,재사용성) & 개별 쿼리가 아닌 쿼리 캐시에 에러 핸들러를 연결하여 전역 오류 핸들러 지정 & fallback 데이터(map-undefined data err.. 해결)

--- lazy-days-spa/client/src/components/staff/hooks/useStaff.ts
+++ lazy-days-spa/client/src/components/staff/hooks/useStaff.ts
@@ -1,15 +1,17 @@
 import { Dispatch, SetStateAction, useState } from 'react';
+import { useQuery } from 'react-query';
 
 import type { Staff } from '../../../../../shared/types';
 import { axiosInstance } from '../../../axiosInstance';
 import { queryKeys } from '../../../react-query/constants';
 import { filterByTreatment } from '../utils';
+// import { useQuery } from '@chakra-ui/react';
 
 // for when we need a query function for useQuery
-// async function getStaff(): Promise<Staff[]> {
-//   const { data } = await axiosInstance.get('/staff');
-//   return data;
-// }
+async function getStaff(): Promise<Staff[]> {
+  const { data } = await axiosInstance.get('/staff');
+  return data;
+}
 
 interface UseStaff {
   staff: Staff[];
@@ -18,11 +20,12 @@ interface UseStaff {
 }
 
 export function useStaff(): UseStaff {
-  // for filtering staff by treatment
+  const fallback = [];
   const [filter, setFilter] = useState('all');
 
   // TODO: get data from server via useQuery
-  const staff = [];
+  const { data: staff = fallback } = useQuery(queryKeys.staff, getStaff); // 키, 쿼리함수
+  // 구조 분해 프로퍼티의 이름을 data에서 staff로 바꿔서, 반환 객체에 staff를 반환할 수 있도록 만듦
 
   return { staff, filter, setFilter };
 }
--- lazy-days-spa/client/src/components/treatments/hooks/useTreatments.ts
+++ lazy-days-spa/client/src/components/treatments/hooks/useTreatments.ts
@@ -1,15 +1,33 @@
+import { useQuery } from 'react-query';
+
 import type { Treatment } from '../../../../../shared/types';
 import { axiosInstance } from '../../../axiosInstance';
 import { queryKeys } from '../../../react-query/constants';
-import { useCustomToast } from '../../app/hooks/useCustomToast';
+// 전역 오류 핸들러 지정
+// import { useCustomToast } from '../../app/hooks/useCustomToast';
 
 // for when we need a query function for useQuery
-// async function getTreatments(): Promise<Treatment[]> {
-//   const { data } = await axiosInstance.get('/treatments');
-//   return data;
-// }
+async function getTreatments(): Promise<Treatment[]> {
+  const { data } = await axiosInstance.get('/treatments');
+  return data;
+}
 
 export function useTreatments(): Treatment[] {
-  // TODO: get data from server via useQuery
-  return [];
+  // 모든 Toast에 스타이링을 추가하기 때문에 그러면 매번 스타일링을 추가할 필요 없다
+  // const toast = useCustomToast(); // onError 콜백에 사용할 수 있는 toast 함수
+
+  const fallback = [];
+  const { data = fallback } = useQuery(
+    queryKeys.treatments,
+    getTreatments,
+    //   , {
+    //   onError: (error) => {
+    //     // 만약 err가 Js Error 클래스의 인스턴스라면
+    //     const title = error instanceof Error ? error.message : 'error on Server';
+    //     toast({ title, status: 'error' });
+    //   },
+    // }
+  );
+
+  return data;
 }
